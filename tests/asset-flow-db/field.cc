63b1f9ac8829ca4178107b8d547f1d908cf85153
Gabriel Valderramos <gabrielvalderramos@gmail.com>
2024-01-31 11:41:39 -0800

    Removed the testing for now

--- tests/asset-flow-db/field.cc
+++ tests/asset-flow-db/field.cc
@@ -1,50 +1,50 @@
 #include <gtest/gtest.h>
 
-#include "asset-flow-db/field.hpp"
+// #include "asset-flow-db/field.hpp"
 
-// Demonstrate some basic assertions.
-TEST(FieldClass, CheckSetValueByEqOperator) {
-    Field<int> f1("field1");
-    f1 = 10;
-    EXPECT_EQ(f1.get(), 10);
-}
+// // Demonstrate some basic assertions.
+// TEST(FieldClass, CheckSetValueByEqOperator) {
+//     Field<int> f1("field1");
+//     f1 = 10;
+//     EXPECT_EQ(f1.get(), 10);
+// }
 
-TEST(FieldClass, CheckSetValueByFunction) {
-    Field<int> f1("field1");
-    f1.set(20);
-    EXPECT_EQ(f1.get(), 20);
-}
+// TEST(FieldClass, CheckSetValueByFunction) {
+//     Field<int> f1("field1");
+//     f1.set(20);
+//     EXPECT_EQ(f1.get(), 20);
+// }
 
-TEST(FieldClass, CheckEqFieldClass) {
-    Field<int> f1("field1");
-    f1.set(20);
-    Field<int> f2("field2");
-    f2 = 20;
+// TEST(FieldClass, CheckEqFieldClass) {
+//     Field<int> f1("field1");
+//     f1.set(20);
+//     Field<int> f2("field2");
+//     f2 = 20;
 
-    auto res = f1 == f2;
-    EXPECT_TRUE(res.result);
+//     auto res = f1 == f2;
+//     EXPECT_TRUE(res.result);
 
-}
-TEST(FieldClass, CheckEqValue) {
-    Field<int> f1("field1");
-    f1.set(20);
+// }
+// TEST(FieldClass, CheckEqValue) {
+//     Field<int> f1("field1");
+//     f1.set(20);
 
-    auto res = f1 == 20;
-    EXPECT_TRUE(res.result);
-}
-TEST(FieldClass, CheckNonEqFieldClass) {
-    Field<int> f1("field1");
-    f1.set(20);
-    Field<int> f2("field2");
-    f2 = 20;
+//     auto res = f1 == 20;
+//     EXPECT_TRUE(res.result);
+// }
+// TEST(FieldClass, CheckNonEqFieldClass) {
+//     Field<int> f1("field1");
+//     f1.set(20);
+//     Field<int> f2("field2");
+//     f2 = 20;
 
-    auto res = f1 != f2;
-    EXPECT_FALSE(res.result);
-}
-TEST(FieldClass, CheckNonEqValue) {
-    Field<int> f1("field1");
-    f1.set(20);
+//     auto res = f1 != f2;
+//     EXPECT_FALSE(res.result);
+// }
+// TEST(FieldClass, CheckNonEqValue) {
+//     Field<int> f1("field1");
+//     f1.set(20);
 
-    auto res = f1 != 20;
-    EXPECT_FALSE(res.result);
-}
+//     auto res = f1 != 20;
+//     EXPECT_FALSE(res.result);
+// }
